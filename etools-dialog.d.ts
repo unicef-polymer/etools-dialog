af3d5b5022a7185fa9d4088c214dc454a8b031ac
Adriana Trif <trif.corina.adriana@gmail.com>
2019-04-10 17:42:11 +0300

    Adjust d.ts

--- etools-dialog.d.ts
+++ etools-dialog.d.ts
@@ -13,8 +13,9 @@
 // tslint:disable:no-any describes the API as best we are able today
 
 import {DialogSpinnerMixin} from './dialog-spinner-mixin.js';
+import { PolymerElement } from '@polymer/polymer';
 
-declare class EtoolsDialog extends DialogSpinnerMixin(Object) {
+declare class EtoolsDialog extends DialogSpinnerMixin(PolymerElement) {
   dialogTitle: string|null|undefined;
   okBtnText: string|null|undefined;
   cancelBtnText: string|null|undefined;
