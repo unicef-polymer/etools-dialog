676786a7d05dee72249d290c03d77440fe944c52
Mercea Emil <office@insightfeatures.com>
2022-10-25 17:14:38 +0300

    [ch31393] Missing Russian translations part II (Partners)

---
+++ assets/translations.js
@@ -0,0 +1,25 @@
+const translations = {
+  en: {
+    SAVING_DATA: 'Saving data...'
+  },
+  es: {
+    SAVING_DATA: 'Guardando datos...'
+  },
+  fr: {
+    SAVING_DATA: 'Enregistrement des données...'
+  },
+  pt: {
+    SAVING_DATA: 'Salvando dados'
+  },
+  ru: {
+    SAVING_DATA: 'Сохранение…'
+  },
+  ro: {
+    SAVING_DATA: 'Se salveaza datele...'
+  },
+  ar: {
+    SAVING_DATA: 'حفظ البيانات...'
+  }
+};
+
+export default translations;
