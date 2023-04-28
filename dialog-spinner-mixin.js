5990c899af669ce287a971515f0e95f94354a959
Adriana Trif <trif.corina.adriana@gmail.com>
2023-04-28 23:06:05 +0300

    [ch33668]Lang init fix (7.0.1)

--- dialog-spinner-mixin.js
+++ dialog-spinner-mixin.js
@@ -5,67 +5,67 @@ import {getTranslation} from './utils/translate';
  * @mixinFunction
  * @demo demo/index-spinner.html
  */
-export const DialogSpinnerMixin = baseClass => class extends baseClass {
-  static get properties() {
-    return {
-      keepDialogOpen: {
-        type: Boolean,
-        reflect: true,
-        attribute: 'keep-dialog-open'
-      },
-      showSpinner: {
-        type: Boolean,
-        reflect: true,
-        attribute: 'show-spinner'
-      },
-      spinnerText: {
-        type: String,
-        attribute: 'spinner-text'
-      },
-      language: {
-        type: String
+export const DialogSpinnerMixin = (baseClass) =>
+  class extends baseClass {
+    static get properties() {
+      return {
+        keepDialogOpen: {
+          type: Boolean,
+          reflect: true,
+          attribute: 'keep-dialog-open'
+        },
+        showSpinner: {
+          type: Boolean,
+          reflect: true,
+          attribute: 'show-spinner'
+        },
+        spinnerText: {
+          type: String,
+          attribute: 'spinner-text'
+        },
+        language: {
+          type: String
+        }
+      };
+    }
+
+    constructor() {
+      super();
+      this.keepDialogOpen = false;
+      this.showSpinner = false;
+      if (!this.language) {
+        this.language = window.EtoolsLanguage || 'en';
       }
-    };
-  }
-
-  constructor() {
-    super();
-    this.keepDialogOpen = false;
-    this.showSpinner = false;
-    if (!this.language) {
-      this.language = window.localStorage.defaultLanguage || 'en';
+      this.spinnerText = getTranslation(this.language, 'SAVING_DATA');
     }
-    this.spinnerText = getTranslation(this.language, 'SAVING_DATA');
-  }
 
-  connectedCallback() {
-    super.connectedCallback();
-    document.addEventListener('language-changed', this.handleLanguageChange.bind(this));
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    document.removeEventListener('language-changed', this.handleLanguageChange.bind(this));
-  }
-
-  handleLanguageChange(e) {
-    this.language = e.detail.language;
-  }
-
-
-  _confirmBtClicked() {
-    if (this.keepDialogOpen) {
-      this.dispatchEvent(new CustomEvent('confirm-btn-clicked', {bubbles: true, composed: true}));
+    connectedCallback() {
+      super.connectedCallback();
+      document.addEventListener('language-changed', this.handleLanguageChange.bind(this));
     }
-  }
 
-  startSpinner() {
-    this.disableConfirmBtn = true;
-    this.showSpinner = true;
-  }
+    disconnectedCallback() {
+      super.disconnectedCallback();
+      document.removeEventListener('language-changed', this.handleLanguageChange.bind(this));
+    }
 
-  stopSpinner() {
-    this.disableConfirmBtn = false;
-    this.showSpinner = false;
-  }
-};
+    handleLanguageChange(e) {
+      this.language = e.detail.language;
+    }
+
+    _confirmBtClicked() {
+      if (this.keepDialogOpen) {
+        this.dispatchEvent(new CustomEvent('confirm-btn-clicked', {bubbles: true, composed: true}));
+      }
+    }
+
+    startSpinner() {
+      this.disableConfirmBtn = true;
+      this.showSpinner = true;
+    }
+
+    stopSpinner() {
+      this.disableConfirmBtn = false;
+      this.showSpinner = false;
+    }
+  };
