8ef4de6d17bd8a5f91da5e1975a994494b5413bc
Trif Adriana <trif.corina.adriana@gmail.com>
2023-04-28 14:36:47 +0300

    Linting

--- dynamic-dialog-mixin.js
+++ dynamic-dialog-mixin.js
@@ -14,77 +14,77 @@ import './etools-dialog.js';
  * @demo demo/index-dynamic.html
  * !!! DEPRECATED - use the exported methods form dynamic-dialog.js
  */
-export const DynamicDialogMixin = baseClass => class extends EtoolsLogsMixin(baseClass) {
-
-  createDialog(title, size, okBtnText, cancelBtnText, closeCallback, content, removePadding, theme) {
-    const config = {
-      title: title,
-      size: size,
-      okBtnText: okBtnText,
-      cancelBtnText: cancelBtnText,
-      closeCallback: closeCallback,
-      content: content,
-      noPadding: removePadding,
-      theme: theme
-    };
-    const dialog = this.createDynamicDialog(config);
-    document.querySelector('body').appendChild(dialog);
-    return dialog;
-  }
-
-  createDynamicDialog(config) {
-    if (!this._validateParams(config)) {
-      return null;
+export const DynamicDialogMixin = (baseClass) =>
+  class extends EtoolsLogsMixin(baseClass) {
+    createDialog(title, size, okBtnText, cancelBtnText, closeCallback, content, removePadding, theme) {
+      const config = {
+        title: title,
+        size: size,
+        okBtnText: okBtnText,
+        cancelBtnText: cancelBtnText,
+        closeCallback: closeCallback,
+        content: content,
+        noPadding: removePadding,
+        theme: theme
+      };
+      const dialog = this.createDynamicDialog(config);
+      document.querySelector('body').appendChild(dialog);
+      return dialog;
     }
 
-    const dialog = document.createElement('etools-dialog');
-    this._applyDefaultDialogConfig(dialog);
+    createDynamicDialog(config) {
+      if (!this._validateParams(config)) {
+        return null;
+      }
 
-    for (const propertyName in config) {
-      if (!Object.prototype.hasOwnProperty.call(config, propertyName) || propertyName === 'closeCallback') {
-        continue;
+      const dialog = document.createElement('etools-dialog');
+      this._applyDefaultDialogConfig(dialog);
+
+      for (const propertyName in config) {
+        if (!Object.prototype.hasOwnProperty.call(config, propertyName) || propertyName === 'closeCallback') {
+          continue;
+        }
+        if (propertyName === 'noPadding' && typeof config[propertyName] === 'boolean') {
+          dialog.noPadding = config.noPadding;
+          continue;
+        }
+        if (typeof config[propertyName] === 'string' && config[propertyName] !== '') {
+          dialog[propertyName] = config[propertyName];
+        }
       }
-      if (propertyName === 'noPadding' && typeof config[propertyName] === 'boolean') {
-        dialog.noPadding = config.noPadding;
-        continue;
+      // set close callback
+      if (config.closeCallback) {
+        dialog.addEventListener('close', function (event) {
+          config.closeCallback(event);
+        });
       }
-      if (typeof config[propertyName] === 'string' && config[propertyName] !== '') {
-        dialog[propertyName] = config[propertyName];
-      }
+
+      document.querySelector('body').appendChild(dialog);
+      setTimeout(() => {
+        const msgPlaceholder = dialog.shadowRoot.querySelector('#dynamicContent');
+        if (msgPlaceholder) {
+          msgPlaceholder.appendChild(config.content);
+        }
+      }, 400);
+      return dialog;
     }
-    // set close callback
-    if (config.closeCallback) {
-      dialog.addEventListener('close', function(event) {
-        config.closeCallback(event);
-      });
-    }
 
-    document.querySelector('body').appendChild(dialog);
-    setTimeout(() => {
-      const msgPlaceholder = dialog.shadowRoot.querySelector('#dynamicContent');
-      if (msgPlaceholder) {
-        msgPlaceholder.appendChild(config.content);
+    _validateParams(config) {
+      if (typeof config.content === 'undefined' || config.content === null) {
+        // eslint-disable-next-line no-console
+        this.logError('[DynamicDialogBehavior] You must provide a valid dialog content');
+        return false;
       }
-    }, 400);
-    return dialog;
-  }
-
-  _validateParams(config) {
-    if (typeof config.content === 'undefined' || config.content === null) {
-      // eslint-disable-next-line no-console
-      this.logError('[DynamicDialogBehavior] You must provide a valid dialog content');
-      return false;
+      return true;
     }
-    return true;
-  }
 
-  _applyDefaultDialogConfig(dialog) {
-    dialog.theme = 'confirmation';
-    dialog.okBtnText = 'Yes';
-    dialog.cancelBtnText = 'No';
-  }
+    _applyDefaultDialogConfig(dialog) {
+      dialog.theme = 'confirmation';
+      dialog.okBtnText = 'Yes';
+      dialog.cancelBtnText = 'No';
+    }
 
-  removeDialog(dialogElement) {
-    document.querySelector('body').removeChild(dialogElement);
-  }
-};
+    removeDialog(dialogElement) {
+      document.querySelector('body').removeChild(dialogElement);
+    }
+  };
